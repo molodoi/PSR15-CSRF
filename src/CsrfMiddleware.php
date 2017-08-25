d6c2059b50ebc7f033942fc17491fada1863175c
Matthieu Lamerant <matthieu.lamerant-isc.France@ibm.com>
2017-08-25 15:43:24 +0200

    Test 23:35

--- src/CsrfMiddleware.php
+++ src/CsrfMiddleware.php
@@ -9,9 +9,56 @@ use Psr\Http\Message\ServerRequestInterface;
 
 class CsrfMiddleware implements MiddlewareInterface
 {
+
+    /**
+     * @var array|\ArrayAccess
+     */
+    private $session;
+    /**
+     * @var string
+     */
+    private $formKey;
+
+
+    /**
+     * CsrfMiddleware constructor.
+     * @param array|\ArrayAccess $session
+     * @param string $sessionKey
+     * @param string $formKey
+     */
+    public function __construct(&$session, $sessionKey = 'csrf.tokens', $formKey = '_csrf')
+    {
+        $this->session = $session;
+        $this->sessionKey = $sessionKey;
+        $this->formKey = $formKey;
+    }
+
+    private $sessionKey;
+
+    /**
+     * @param ServerRequestInterface $request
+     * @param DelegateInterface $delegate
+     * @return ResponseInterface
+     */
     public function process(ServerRequestInterface $request, DelegateInterface $delegate)
     {
-        // TODO: Implement process() method.
+        if(in_array($request->getMethod(), ['PUT', 'POST', 'DELETE'])){
+
+        }
+        return $delegate->process($request);
+    }
+
+    /**
+     * Generate and store a random token
+     * @return string
+     */
+    public function generateToken()
+    {
+        $token = bin2hex(random_bytes(16));
+        $tokens = $this->session['csrf.tokens'] ?? [];
+        $tokens[] = $token;
+        $this->session['csrf.tokens'] = $tokens;
+        return $token;
     }
 
 }
--- tests/CsrfMiddleWareTest.php
+++ tests/CsrfMiddleWareTest.php
@@ -8,27 +8,89 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Ticme\Csrf\CsrfMiddleware;
 
-class CsrfMiddleWareTest extends TestCase {
+class CsrfMiddleWareTest extends TestCase
+{
 
-    public function makeRequest($method = 'GET'){
+
+
+    private function makeMiddleware($session = []){
+        return new CsrfMiddleware($session);
+    }
+
+    private function makeRequest($method = 'GET', $params = null){
         $request = $this->getMockBuilder(ServerRequestInterface::class)
             ->getMock();
         $request->method('getMethod')->willReturn($method);
+        $request->method('getParsedBody')->willReturn($params);
         return $request;
     }
 
-    public function makeDelegate(){
-        $request = $this->getMockBuilder(DelegateInterface::class)->getMock();
-        $response = $this->getMockBuilder(ResponseInterface::class)->getMock();
-        $request->method('process')->willReturn($method);
-        return $request;
+    private function makeDelegate(){
+        $delegate = $this->getMockBuilder(DelegateInterface::class)->getMock();
+        $delegate->method('process')->willReturn($this->makeResponse());
+        return $delegate;
     }
 
-    public function testGetPass(){
-        $middleware = new CsrfMiddleware();
+    private function makeResponse(){
+        return $this->getMockBuilder(ResponseInterface::class)->getMock();
+    }
+
+    private function testAcceptValidSession(){
+        $a = [];
+        $b = $this->getMockBuilder(\ArrayAccess::class)->getMock();
+        $middlewareA = $this->makeMiddleware($a);
+        $middlewareB = $this->makeMiddleware($b);
+        $this->assertInstanceOf(CsrfMiddleware::class, $middlewareA);
+        $this->assertInstanceOf(CsrfMiddleware::class, $middlewareB);
+    }
+
+    private function testRejectInvalidSession(){
+        $this->expectException();
+        $middlewareA = $this->makeMiddleware(new \stdClass());
+    }
+
+    private function testGetPass(){
+        $middleware = $this->makeMiddleware();
+        $delegate = $this->makeDelegate();
+        $delegate->expects($this->once())->method('process');
         $middleware->process(
             $this->makeRequest('GET'),
+            $delegate
+        );
+    }
 
+    private function testPreventPost(){
+        $middleware = $this->makeMiddleware();
+        $delegate = $this->makeDelegate();
+        $delegate->expects($this->never())->method('process');
+        $this->expectException(\Exception::class);
+        $middleware->process(
+            $this->makeRequest('POST'),
+            $delegate
+        );
+    }
+
+    private function testPostSuccessfullyWithToken(){
+        $middleware = $this->makeMiddleware();
+        $token = $middleware->generateToken();
+        $delegate = $this->makeDelegate();
+        $delegate->expects($this->once())->method('process')->willReturn($this->makeResponse());
+        $this->expectException(\Exception::class);
+        $middleware->process(
+            $this->makeRequest('POST', ['_csrf' => $token]),
+            $delegate
+        );
+    }
+
+    private function testPostWithInvalidToken(){
+        $middleware = $this->makeMiddleware();
+        $token = $middleware->generateToken();
+        $delegate = $this->makeDelegate();
+        $delegate->expects($this->once())->method('process')->willReturn($this->makeResponse());
+        $this->expectException(\Exception::class);
+        $middleware->process(
+            $this->makeRequest('POST', ['_csrf' => 'badtoken']),
+            $delegate
         );
     }
 }
